6be344da6d344661ed46acc15ac15f65a9834b14
Fabien LH <fabien.lehouedec@gmail.com>
2021-05-27 01:14:33 +0200

    Migrating to lit.dev

--- src/components/hello-world.ts
+++ src/components/hello-world.ts
@@ -1,4 +1,5 @@
-import { LitElement, html, property, customElement } from 'lit-element'
+import { LitElement, html } from 'lit'
+import { customElement, property } from 'lit/decorators'
 
 @customElement('hello-world')
 export class HelloWorldElement extends LitElement {
